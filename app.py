596bd49a97e0f402d5059ac278e135040a99d290
lizhengsh <lizhengsh@gmail.com>
2022-05-17 22:16:01 +0800

    Add index and template pages

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask, escape, url_for
+from flask import Flask, escape, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Welcome here!'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
-@app.route('/user/<name>')
-def user_name(name):    
-    return 'Welecome %s' % escape(name)
 
-@app.route('/test')
-def test_url_for():
-    print(url_for('user_name', name='request'))
-    return 'Test git pull request page'
+name = 'madapapa'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
